79614e9d039dca8fd3b00e3c7b0b6bbdfa264edc
Youkwhan <Youkwhan@gmail.com>
2023-04-18 14:48:34 -0400

    new images and fix grid img layout

--- data.js
+++ data.js
@@ -2,19 +2,19 @@ const postData = [
    {
       name:"Overcoming Common Challenges in Learning to Code",
       date: "APRIL 23 2023",
-      image: "#",
+      image: "blog-image-04.jpg",
       text: "To learn to code, start with the basics of a programming language, practice consistently, aand gradually work on more complex projects to improve your skills."
    },
    {
       name: "Maximizing Your Workspace",
       date: "APRIL 22, 2023",
-      image: "#",
+      image: "blog-image-05.jpg",
       text: "To maximize your workspace, use a clean and organized setup that is comfortable and easy to use to mimize distractions."
    },
    {
       name: "The Fundamentals of CSS",
       date: "APRIL 20, 2023",
-      image: "#",
+      image: "blog-image-06.jpg",
       text:"CSS includes understanding selectors, properties, and values, which can be used to style HTML elements and create visually appealing and responsive web pages."
    }
 ]
